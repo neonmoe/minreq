f8004aeb6d143e6bf574ae878d54ce9bdddbfb16
Jens Pitkanen <jens@neon.moe>
2022-01-06 03:01:00 +0200

    Bump to dev version

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! minreq = { version = "2.5.0", features = ["punycode"] }
+//! minreq = { version = "2.5.1-alpha.0", features = ["punycode"] }
 //! ```
 //!
 //! Below is the list of all available features.
