19546cc5c834be21039b6819615d295c1c2e15af
Jens Pitkanen <jens@neon.moe>
2023-08-24 19:22:05 +0300

    Release 2.9.0

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! minreq = { version = "2.8.2-alpha", features = ["punycode"] }
+//! minreq = { version = "2.9.0", features = ["punycode"] }
 //! ```
 //!
 //! Below is the list of all available features.
